7c6c69801cfaff332a2c64546d483c33537a874e
Jakub Dolejs <jakubdolejs@gmail.com>
2025-08-14 20:30:50 +0100

    Added FaceDoesNotMatchExisting exception

--- lib/src/main/java/com/appliedrec/verid3/facetemplateregistry/FaceTemplateRegistryException.kt
+++ lib/src/main/java/com/appliedrec/verid3/facetemplateregistry/FaceTemplateRegistryException.kt
@@ -1,9 +1,21 @@
 package com.appliedrec.verid3.facetemplateregistry
 
+import com.appliedrec.verid3.common.FaceTemplate
+
 sealed class FaceTemplateRegistryException(message: String) : Exception(message) {
-    data class SimilarFaceAlreadyRegistered(val registeredIdentifier: String) : FaceTemplateRegistryException("Similar face already registered as $registeredIdentifier")
-    data class IdentifierNotRegistered(val identifier: String) : FaceTemplateRegistryException("Identifier not registered: $identifier")
+    data class SimilarFaceAlreadyRegistered(
+        val registeredIdentifier: String,
+        val faceTemplate: FaceTemplate<*, *>,
+        val score: Float
+    ) : FaceTemplateRegistryException("Similar face already registered as $registeredIdentifier")
+    data class IdentifierNotRegistered(
+        val identifier: String
+    ) : FaceTemplateRegistryException("Identifier not registered: $identifier")
     object IncompatibleFaceTemplates : FaceTemplateRegistryException("Incompatible face templates") {
         private fun readResolve(): Any = IncompatibleFaceTemplates
     }
+    data class FaceDoesNotMatchExisting(
+        val faceTemplate: FaceTemplate<*, *>,
+        val maxScore: Float
+    ) : FaceTemplateRegistryException("Face does not match registered face templates(s)")
 }
